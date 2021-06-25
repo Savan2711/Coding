4b6b1e95a227c826e079956732f5d60896aae27e
Savan2711 <savananadani@gmail.com>
2021-06-25 16:06:57 +0530

    Basic fib with DP

---
+++ DP/Fibonacci Number.cpp
@@ -0,0 +1,24 @@
+/*
+https://leetcode.com/problems/fibonacci-number/
+*/
+
+class Solution {
+public:
+    int fib(int n) {
+        
+        int a = 0, b = 1;
+        if(n == 0)
+        {return a;}
+        else if(n == 1)
+        {return b;}
+        
+        for(int i = 2; i <= n; i++)
+        {
+            b += a;
+            a = b - a;
+        }
+        
+        return b;
+        
+    }
+};
