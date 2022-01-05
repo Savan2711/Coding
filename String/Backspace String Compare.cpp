8c86472bf9a55cb98e16458b96d4ab2017fdc13c
Savan2711 <savananadani@gmail.com>
2022-01-05 10:06:00 +0530

    Update Backspace String Compare

--- String/Backspace String Compare.cpp
+++ String/Backspace String Compare.cpp
@@ -2,6 +2,7 @@
 https://leetcode.com/problems/backspace-string-compare/
 */
 
+/*Solution for getting string by adjusting backspace*/
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
@@ -36,5 +37,59 @@ public:
             
         }
         return sback == tback;
+    }
+};
+
+/*Solution using two pointer*/
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        
+        int sbcnt = 0, tbcnt = 0;
+        string sback = "";
+        
+        int i = s.length() - 1, j = t.length() - 1;
+        
+        while(i >= 0 || j >= 0)
+        {
+            while(i >= 0)
+            {
+                if(s[i] == '#')
+                {
+                    sbcnt++;
+                    i--;
+                }
+                else if(sbcnt)
+                {
+                    sbcnt--;
+                    i--;
+                }
+                else break;
+            }
+            
+            while(j >= 0)
+            {
+                if(t[j] == '#')
+                {
+                    tbcnt++;
+                    j--;
+                }
+                else if(tbcnt)
+                {
+                    tbcnt--;
+                    j--;
+                }
+                else break;
+            }
+            
+            if(i >= 0 && j >= 0)
+            {
+                if(s[i] != t[j]) return false;
+            }
+            if((i >= 0) != (j >= 0)) return false;
+            i--;
+            j--;
+        }
+        return true;
     }
 };
