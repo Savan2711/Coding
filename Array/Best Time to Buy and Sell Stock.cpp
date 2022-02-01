1edf88368dde9b9bac1bc1fe2db192ca33c339c5
Savan2711 <savananadani@gmail.com>
2022-02-01 09:10:45 +0530

    121. Best Time to Buy and Sell Stock

--- Array/Best Time to Buy and Sell Stock.cpp
+++ Array/Best Time to Buy and Sell Stock.cpp
@@ -2,6 +2,25 @@
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 */
 
+/* Using two pointers (i.e., buy and sell) */
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int profit = 0, buy = 0, sell = 1;
+        while(buy < prices.size() - 1 && sell < prices.size())
+        {
+            profit = max(profit, prices[sell] - prices[buy]);
+            if(prices[sell] < prices[buy])
+            {buy = sell;}
+            sell++;
+        }
+        return profit;
+        
+    }
+};
+
+/* Using Kadane's algorithm */
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
