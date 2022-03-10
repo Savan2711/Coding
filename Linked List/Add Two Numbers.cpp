861060677051677e55aa7ea8dfdacc8cbc12650f
Savan2711 <savananadani@gmail.com>
2022-03-10 10:03:42 +0530

    Adding Recursive solution

--- Linked List/Add Two Numbers.cpp
+++ Linked List/Add Two Numbers.cpp
@@ -2,6 +2,7 @@
 https://leetcode.com/problems/add-two-numbers/
 */
 
+/* Iterative solution */
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -59,5 +60,36 @@ public:
             cur = NULL;
         }
         return ans;
+    }
+};
+
+/* Recursive solution */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int carry = 0;
+    
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        if(!(l1 -> next)  && !(l2 -> next))
+        {
+            return new ListNode((l1 -> val + l2 -> val + carry) % 10, (l1 -> val + l2 -> val + carry) / 10 > 0 ? new ListNode((l1 -> val + l2 -> val + carry) / 10) : NULL);
+        }
+        
+        int v = (l1 -> val + l2 -> val + carry) % 10;
+        carry = (l1 -> val + l2 -> val + carry) / 10;
+        return new ListNode(v, addTwoNumbers(l1 -> next ? l1 -> next : new ListNode(), l2 -> next ? l2 -> next : new ListNode()));
+        
+        
     }
 };
