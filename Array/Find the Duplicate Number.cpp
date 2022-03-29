1d027fdfb278ca061c91c0814166abe125b4ca63
Savan2711 <savananadani@gmail.com>
2022-03-29 09:13:41 +0530

    287. Find the Duplicate Number

---
+++ Array/Find the Duplicate Number.cpp
@@ -0,0 +1,29 @@
+/*
+https://leetcode.com/problems/find-the-duplicate-number/
+*/
+
+class Solution {
+public:
+    
+    int findDuplicate(vector<int>& nums) {
+        
+        int fast = nums[0], slow = nums[0];
+        
+        do
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(fast != slow);    
+        
+        fast = nums[0];
+        
+        while(fast != slow)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return fast;
+        
+    }
+};
