17ef448a8515a1af6026e4c5f7446944c183f7c6
Savan2711 <savananadani@gmail.com>
2023-03-16 21:30:53 +0530

    283. Move Zeroes

---
+++ Array/Move Zeroes.cpp
@@ -0,0 +1,19 @@
+/*
+https://leetcode.com/problems/move-zeroes/description/
+*/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int idx = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] != 0)
+            {
+                nums[idx] = nums[i];
+                idx++;
+            }
+        }
+        for(int i = idx; i < nums.size(); i++) nums[i] = 0;
+    }
+};
